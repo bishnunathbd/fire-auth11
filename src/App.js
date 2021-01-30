a3f38b212736fea9f152c672af58452ba8b8ea9f
bishnunathbd <bishnunath774@gmail.com>
2021-01-30 12:22:44 +0600

    41-7-updated

--- src/App.js
+++ src/App.js
@@ -16,30 +16,35 @@ function App() {
 
   // Google sign in 
   const googleProvider = new firebase.auth.GoogleAuthProvider();
+
   const handleGoogleSignIn = () => {
     firebase.auth().signInWithPopup(googleProvider)
-    .then(res => {
-      const {displayName, photoURL, email} = res.user;
-      const signedInUser = {
-        isSignedIn: true,
-        name: displayName,
-        email: email,
-        photo: photoURL
-      };
-      setUser(signedInUser);
-    })
+      .then(res => {
+        const { displayName, photoURL, email } = res.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(signedInUser);
+      })
+      .then(err => {
+        console.log(err);
+        console.log(err.message);
+      })
   }
 
   return (
     <div className="App">
       <h2>React Authentication Using Firebase</h2>
-      <br/>
+      <br />
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
       {
         user.isSignedIn && <div>
           <p>Welcome, {user.name}</p>
           <p>Your Email: {user.email}</p>
-          <img src={user.photo} style={{width: '25%'}} alt=""/>
+          <img src={user.photo} style={{ width: '25%' }} alt="" />
         </div>
       }
     </div>
