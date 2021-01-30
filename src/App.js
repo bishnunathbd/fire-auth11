497ef8b790ac580a07e68d423d53050406202ee4
bishnunathbd <bishnunath774@gmail.com>
2021-01-30 12:18:58 +0600

    41-7-set loggedIn user info in state & display userInfo

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import './App.css';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
+import { useState } from 'react';
 
 firebase.initializeApp(firebaseConfig);
 
 function App() {
-  
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  });
+
   // Google sign in 
   const googleProvider = new firebase.auth.GoogleAuthProvider();
   const handleGoogleSignIn = () => {
     firebase.auth().signInWithPopup(googleProvider)
     .then(res => {
       const {displayName, photoURL, email} = res.user;
-      console.log(displayName, photoURL, email);
+      const signedInUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email,
+        photo: photoURL
+      };
+      setUser(signedInUser);
     })
   }
 
@@ -22,6 +35,13 @@ function App() {
       <h2>React Authentication Using Firebase</h2>
       <br/>
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {
+        user.isSignedIn && <div>
+          <p>Welcome, {user.name}</p>
+          <p>Your Email: {user.email}</p>
+          <img src={user.photo} style={{width: '25%'}} alt=""/>
+        </div>
+      }
     </div>
   );
 }
