d5dd30ae0c55265417bce7d5dbff5dda408fa67a
bishnunathbd <bishnunath774@gmail.com>
2021-01-30 11:48:25 +0600

    41-6-sign in with google

--- src/App.js
+++ src/App.js
@@ -6,9 +6,22 @@ import firebaseConfig from './firebase.config';
 firebase.initializeApp(firebaseConfig);
 
 function App() {
+  
+  // Google sign in 
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    firebase.auth().signInWithPopup(googleProvider)
+    .then(res => {
+      const {displayName, photoURL, email} = res.user;
+      console.log(displayName, photoURL, email);
+    })
+  }
+
   return (
     <div className="App">
       <h2>React Authentication Using Firebase</h2>
+      <br/>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }
