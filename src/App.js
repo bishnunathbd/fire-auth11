89f9ff28655e3a924a58f146d65fb2c788540e2b
bishnunathbd <bishnunath774@gmail.com>
2021-02-04 12:12:50 +0600

    42-8-how to handle fb login

--- src/App.js
+++ src/App.js
@@ -41,6 +41,34 @@ function App() {
       })
   }
 
+
+  // Facebook sign in
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
+
+  const handleFbSignIn = () => {
+    firebase.auth().signInWithPopup(fbProvider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+        // The signed-in user info.
+        var user = result.user;
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var accessToken = credential.accessToken;
+        console.log('fb user after sign in', user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(errorMessage);
+      });
+  }
+
+
   // sign out
   const handleSignOut = () => {
     firebase.auth().signOut()
@@ -145,6 +173,8 @@ function App() {
           <img src={user.photo} style={{ width: '25%' }} alt="" />
         </div>
       }
+      <br /> <br />
+      <button onClick={handleFbSignIn}>Sign in using Facebook</button>
 
       <h2>Our own Authentication</h2>
       <input type="checkbox" name="newUser" onChange={() => setNewUser(!newUser)} id="" />
